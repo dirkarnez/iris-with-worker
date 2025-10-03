9571b5c04626c8cfcb13d6232b02b6897c3d29ec
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-04 05:16:04 +0800

    - update main.go

--- main.go
+++ main.go
@@ -8,4 +8,14 @@ func main() {
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 	// shutdown when process ends
 	defer queue.ShutDown()
+
+	queue.Add(key)
+	queue.Forget(key)
+	queue.AddRateLimited(key)
+	queue.NumRequeues(key)
+	key, quit := queue.Get()
+	if quit {
+		return false
+	}
+	defer queue.Done(key)
 }
